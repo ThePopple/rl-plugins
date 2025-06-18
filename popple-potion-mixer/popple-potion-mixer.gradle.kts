d6d804adbac9973221abe48c746ffec449fe6173
unknown <jpopple1505@hotmail.co.uk>
2025-06-18 20:32:29 +0100

    feel like I'm going insane

--- popple-potion-mixer/popple-potion-mixer.gradle.kts
+++ popple-potion-mixer/popple-potion-mixer.gradle.kts
@@ -9,7 +9,7 @@ tasks {
         manifest {
             attributes(mapOf(
                     "Plugin-Version" to project.version,
-                "Plugin-Id" to project.extra["PluginId"],
+                    "Plugin-Id" to project.extra["PluginId"],
                     "Plugin-Provider" to project.extra["PluginProvider"],
                     "Plugin-Description" to project.extra["PluginDescription"],
                     "Plugin-License" to project.extra["PluginLicense"]

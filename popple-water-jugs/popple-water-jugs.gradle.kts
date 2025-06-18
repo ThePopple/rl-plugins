32b9c29e58d5f25d88f46075e4c97d72a988c742
unknown <jpopple1505@hotmail.co.uk>
2025-06-18 21:07:36 +0100

    forgot jug buyer ffs

--- popple-water-jugs/popple-water-jugs.gradle.kts
+++ popple-water-jugs/popple-water-jugs.gradle.kts
@@ -8,7 +8,7 @@ tasks {
         manifest {
             attributes(mapOf(
                     "Plugin-Version" to project.version,
-                    "Plugin-Id" to "popple-water-jugs",
+                    "Plugin-Id" to nameToId(project.extra["PluginName"] as String),
                     "Plugin-Provider" to project.extra["PluginProvider"],
                     "Plugin-Description" to project.extra["PluginDescription"],
                     "Plugin-License" to project.extra["PluginLicense"]
